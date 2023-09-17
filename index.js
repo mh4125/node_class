fc7dabda4ef59cbc9a6f1a6ac85d22a0ef667aee
mh4125 <hargrove_michaela@students.columbusstate.edu>
2023-09-17 23:05:54 +0000

    Added ejs Template

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
 
+app.use('/static', express.static('public'));
+app.set("view engine", "ejs");
+
 app.get('/', function( req, res ){
-    res.send('Hello World')
+    res.render('home.ejs');
 })
+
+//app.get('/results', function( req, res ){
+    //change to another ejs file with movie/show results info
+    //res.render('home.ejs');
+//})
+
+//app.get('/', function( req, res ){
+    //change to another ejs file with contact info
+    //res.render('home.ejs');
+//})
 
 app.listen(3000, () => {
     console.log('started on port 3000')
