8bd08c9c6412c81eaa9907d8516bd532e48a37d5
mh4125 <hargrove_michaela@students.columbusstate.edu>
2023-09-18 00:14:57 +0000

    Node Video 3 Question

--- index.js
+++ index.js
@@ -8,16 +8,22 @@ app.get('/', function( req, res ){
     res.render('home.ejs');
 })
 
-//app.get('/results', function( req, res ){
-    //change to another ejs file with movie/show results info
-    //res.render('home.ejs');
-//})
+app.get('/path/:name', function( req, res ){
+    let name = req.params.name
+    console.log(name)
+    res.render('home.ejs');
+})
 
-//app.get('/', function( req, res ){
-    //change to another ejs file with contact info
-    //res.render('home.ejs');
-//})
+app.get('/query', function( req, res ){
+    let name = req.query.name
+    console.log(name)
+    res.render('home.ejs');
+})
 
 app.listen(3000, () => {
     console.log('started on port 3000')
 })
+
+// path param REQUIRED /home/6821868/...
+//query param /home?foo=bar&bar=foo
+//PUT & POST/ CREATE & UPDATE body, from a form
