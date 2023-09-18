ddc26d898ac639d90a073e16a7dca2147674e24c
mh4125 <hargrove_michaela@students.columbusstate.edu>
2023-09-18 18:23:51 +0000

    Improvements after node 3 video

--- index.js
+++ index.js
@@ -5,19 +5,18 @@ app.use('/static', express.static('public'));
 app.set("view engine", "ejs");
 
 app.get('/', function( req, res ){
-    res.render('home.ejs');
+    res.render('home.ejs', {name: null});
 })
 
 app.get('/path/:name', function( req, res ){
-    let name = req.params.name
-    console.log(name)
-    res.render('home.ejs');
+    let name = req.params.name;
+    res.render('home.ejs',{name: name});
 })
 
 app.get('/query', function( req, res ){
-    let name = req.query.name
-    console.log(name)
-    res.render('home.ejs');
+    let name = req.query.name;
+    let nameObject = {"name": name}
+    res.render('home.ejs', nameObject);
 })
 
 app.listen(3000, () => {
